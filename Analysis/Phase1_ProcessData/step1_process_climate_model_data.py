1ecdc8fce015e328102152a67f1a85da3896d066
czarakas <czarakas@uw.edu>
2019-10-31 05:11:09 +0000

    Updated Phase 1 Step 1 to deal with inconsistent coordinate labels

--- Analysis/Phase1_ProcessData/step1_process_climate_model_data.py
+++ Analysis/Phase1_ProcessData/step1_process_climate_model_data.py
@@ -2,10 +2,14 @@
 This module processes raw CMIP6 output to create files with consistent dimensions for all models.
 
 Running this script for all models with just averaging over ensembles, regridding, and saving takes
-X mins.
+about 15-20 mins.
+
+Make sure to clear the directory you would like the zarr files to be saved in - something about
+this format makes it not work to overwrite existing files.
 """
 
-################## Load Packages ######################################## 
+####################### Load Packages ########################################
+
 import time
 import xarray as xr
 import xesmf as xe
@@ -13,15 +17,20 @@ import xesmf as xe
 import analysis_parameters
 import create_data_dict
 
-######### Read in Settings for Data Dictionary#############################
+############ Read in Settings for Data Dictionary#############################
+
 THIS_EXPERIMENT_ID = analysis_parameters.EXPERIMENT_LIST
 THIS_VARIABLE_ID = analysis_parameters.VARIABLE_ID
 THIS_TABLE_ID = analysis_parameters.TABLE_ID
 THIS_GRID_LABEL = analysis_parameters.GRID_LABEL
 OUTPUT_PATH = analysis_parameters.DIR_PROCESSED_DATA
-DIR_INTERMEDIATE = '/home/jovyan/local-climate-data-tool/Data/IntermediateData/'
+DIR_INTERMEDIATE = analysis_parameters.DIR_INTERMEDIATE_DATA
 
-######### Create Data Dictionary to test#################################### 
+########### Create Data Dictionary to test####################################
+
+# each value in this dictionary is an xarray with dimensions:
+# lat, lon, time, member_id
+
 [DATASET_INFO,
  DSET_DICT,
  MODELNAMES] = create_data_dict.create_data_dict(THIS_EXPERIMENT_ID,
@@ -29,7 +38,7 @@ DIR_INTERMEDIATE = '/home/jovyan/local-climate-data-tool/Data/IntermediateData/'
                                                  THIS_TABLE_ID,
                                                  THIS_GRID_LABEL)
 
-######### Create functions for analysis #################################### 
+########### Create functions for analysis ####################################
 
 def create_reference_grid(modelname, experiment_id, activity_id):
     """Creates reference grid to which all model output will be regridded"""
@@ -68,43 +77,48 @@ def process_dataset(this_key):
     # Average over all ensemble members
     ds = ds_original.mean(dim=['member_id'])
 
+    # Rename latitude and longitude coordinate names if necessary
+    if 'latitude' in ds.dims:
+        ds = ds.rename({'longitude': 'lon', 'latitude': 'lat'})
+    else:
+        pass
+
     # Regrid dataset to reference grid
     dataset_regridded = regrid_model(ds, FINAL_GRID)
-    
+
     # Reindex time to consistent time datatype
     # NEED TO DO!
 
     return dataset_regridded
 
 def generate_new_filename(this_key):
-    [_, _, source_id, experiment_id, _, _] = key.split('.')
-    fname = THIS_VARIABLE_ID+'_'+experiment_id+'_'+source_id
-    return fname
+    """Generates filename for processed data"""
+    [_, _, source_id, experiment_id, _, _] = this_key.split('.')
+    this_fname = THIS_VARIABLE_ID+'_'+experiment_id+'_'+source_id
+    return this_fname
 
-def save_dataset(ds, fname):
+def save_dataset(ds, this_fname):
+    """Saves processed dataset as zarr file"""
     ds.load()
-    
     # Export intermediate processed dataset as zarr file
     ds.chunk({'lon':10, 'lat':10, 'time':-1})
-    ds.to_zarr(DIR_INTERMEDIATE+fname+'.zarr')
-    
+    ds.to_zarr(DIR_INTERMEDIATE+this_fname+'.zarr')
 
-################### Main Workflow ##########################################
+
+##################### Main Workflow ##########################################
 
 FINAL_GRID = create_reference_grid(modelname='CAMS-CSM1-0',
                                    activity_id='CMIP',
                                    experiment_id='historical')
-KEY_LIST = [key for key in DSET_DICT.keys()]
 
-start_time = time.time()
-for key in KEY_LIST:
+START_TIME = time.time()
+for key in DSET_DICT.keys():
     # Generate new filename for intermediate processed data
     fname = generate_new_filename(key)
     print(fname)
-    
+
     # Check if one of cases that throws exceptions (to investigate later)
-    if fname in ('tas_historical_CESM2', 
-                 'tas_historical_MCM-UA-1-0',
+    if fname in ('tas_historical_CESM2',
                  'tas_ssp126_CanESM5',
                  'tas_ssp245_CAMS-CSM1-0',
                  'tas_ssp245_HadGEM3-GC31-LL',
@@ -120,7 +134,6 @@ for key in KEY_LIST:
 
         # Save processed data
         save_dataset(ds_processed, fname)
-    elapsed_time = time.time() - start_time
-    print(elapsed_time)
 
 # Print how much time has passed in going through all the files
+print(time.time() - START_TIME)
