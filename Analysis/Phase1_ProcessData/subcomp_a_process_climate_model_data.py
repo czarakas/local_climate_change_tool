9b3a767369b710e1070b2b0a5bfe71f09bc64ab1
czarakas <czarakas@uw.edu>
2019-11-18 22:02:54 +0000

    Added testing for Data Wrangler subcomponent a, sped up code for subcomponent b by chunking model data

--- Analysis/Phase1_ProcessData/subcomp_a_process_climate_model_data.py
+++ Analysis/Phase1_ProcessData/subcomp_a_process_climate_model_data.py
@@ -1,7 +1,8 @@
 """
 This module processes raw CMIP6 output to create files with consistent dimensions for all models.
 
-Running this script for all models with just averaging over ensembles, regridding, and saving takes about 15-20 mins.
+Running this script for all models with just averaging over ensembles, regridding,
+and saving takes about 15-20 mins.
 
 Make sure to clear the directory you would like the zarr files to be saved in - something about
 this format makes it not work to overwrite existing files.
@@ -9,44 +10,24 @@ this format makes it not work to overwrite existing files.
 
 ####################### Load Packages ########################################
 
-import time
 import cftime
 import xarray as xr
 import xesmf as xe
 
 import analysis_parameters
-import subcomp_a_create_data_dict
 
 ############ Read in Settings for Data Dictionary#############################
 
 THIS_EXPERIMENT_ID = analysis_parameters.EXPERIMENT_LIST
 THIS_VARIABLE_ID = analysis_parameters.VARIABLE_ID
-THIS_TABLE_ID = analysis_parameters.TABLE_ID
-THIS_GRID_LABEL = analysis_parameters.GRID_LABEL
 OUTPUT_PATH = analysis_parameters.DIR_PROCESSED_DATA
 DIR_INTERMEDIATE = analysis_parameters.DIR_INTERMEDIATE_DATA
 
-########### Create Data Dictionary to test####################################
-
-# each value in this dictionary is an xarray with dimensions:
-# lat, lon, time, member_id
-
-[DATASET_INFO,
- DSET_DICT,
- MODELNAMES] = subcomp_a_create_data_dict.create_data_dict(THIS_EXPERIMENT_ID,
-                                                 THIS_VARIABLE_ID,
-                                                 THIS_TABLE_ID,
-                                                 THIS_GRID_LABEL)
-
 ########### Create functions for analysis ####################################
 
-def create_reference_grid(modelname, experiment_id, activity_id):
+def create_reference_grid(reference_key, dset_dict):
     """Creates reference grid to which all model output will be regridded"""
-    dataset_info_subset = DATASET_INFO[DATASET_INFO['source_id'] == modelname]
-    institution_id = list(set(dataset_info_subset['institution_id']))[0]
-    nametag = activity_id+'.'+institution_id+'.'+modelname+'.'+experiment_id+\
-              '.'+THIS_TABLE_ID+'.'+THIS_GRID_LABEL
-    thisdata = DSET_DICT[nametag]
+    thisdata = dset_dict[reference_key]
     ds_out = xr.Dataset({'lat': thisdata['lat'],
                          'lon': thisdata['lon']})
     return ds_out
@@ -73,7 +54,7 @@ def regrid_model(ds, reference_grid, latvariable='lat', lonvariable='lon'):
     data_series_regridded.attrs.update(data_series.attrs)
     return data_series_regridded
 
-def process_dataset(this_key):
+def process_dataset(this_key, dset_dict, final_grid):
     """ Processes each dataset in dictionary
     This processing involves:
         (1) Averaging over all ensemble members (member ids)
@@ -82,7 +63,7 @@ def process_dataset(this_key):
         (4) Regridding to reference dataset
     """
     # Get original dataset from dictionary
-    ds_original = DSET_DICT[this_key]
+    ds_original = dset_dict[this_key]
 
     # Average over all ensemble members
     ds = ds_original.mean(dim=['member_id'])
@@ -99,7 +80,7 @@ def process_dataset(this_key):
         pass
 
     # Regrid dataset to reference grid
-    dataset_regridded = regrid_model(ds, FINAL_GRID)
+    dataset_regridded = regrid_model(ds, final_grid)
 
     return dataset_regridded
 
@@ -119,34 +100,18 @@ def save_dataset(ds, this_fname):
 
 ##################### Main Workflow ##########################################
 
-FINAL_GRID = create_reference_grid(modelname='CAMS-CSM1-0',
-                                   activity_id='CMIP',
-                                   experiment_id='historical')
+def process_all_files_in_dictionary(dset_dict, exceptions_list, final_grid):
+    """Add docstring"""
+    for key in dset_dict.keys():
+        # Generate new filename for intermediate processed data
+        fname = generate_new_filename(key)
 
-for key in DSET_DICT.keys():
-    # Generate new filename for intermediate processed data
-    fname = generate_new_filename(key)
+        # Check if one of cases that throws exceptions (to investigate later)
+        if fname in exceptions_list:
+            print('******** skipping ************')
+        else:
+            # Process data
+            ds_processed = process_dataset(key, dset_dict, final_grid)
 
-    # Check if one of cases that throws exceptions (to investigate later)
-    if fname in ('tas_historical_CESM2',
-                 'tas_ssp126_CanESM5',
-                 'tas_ssp126_CAMS-CSM1-0',
-                 
-                 'tas_ssp245_CAMS-CSM1-0',
-                 'tas_ssp245_HadGEM3-GC31-LL',
-                 
-                 'tas_ssp370_CESM2-WACCM',
-                 'tas_ssp370_MPI-ESM1-2-HR',
-                 'tas_ssp370_CAMS-CSM1-0',
-                 'tas_ssp370_BCC-ESM1',
-                 
-                 'tas_ssp585_CAMS-CSM1-0',
-                 'tas_ssp585_CanESM5'
-                ):
-        print('******** skipping ************')
-    else:
-        # Process data
-        ds_processed = process_dataset(key)
-
-        # Save processed data
-        save_dataset(ds_processed, fname)
+            # Save processed data
+            save_dataset(ds_processed, fname)
--- Analysis/Phase1_ProcessData/subcomp_b_multi_model_stats.py
+++ Analysis/Phase1_ProcessData/subcomp_b_multi_model_stats.py
@@ -1,26 +1,32 @@
+"""
+Need to add docstring
+"""
+
 import time
-import pickle
-import analysis_parameters
-import xarray as xr
 import glob
+import xarray as xr
 import numpy as np
-import matplotlib.pyplot as plt
-import multimodelstats as mms
+import analysis_parameters
 
 DATA_PATH = analysis_parameters.DIR_INTERMEDIATE_DATA+'Processed_Model_Data/'
 SCENARIO_LIST = analysis_parameters.EXPERIMENT_LIST
 VARIABLE_NAME = analysis_parameters.VARIABLE_ID
 OUTPUT_PATH = '/home/jovyan/local-climate-data-tool/Data/ProcessedData/'
-INTERMEDIATE_OUTPUT_PATH='/home/jovyan/local-climate-data-tool/Data/IntermediateData/Multi_Model_Stats_Arrays/'
+INTERMEDIATE_OUTPUT_PATH = '/home/jovyan/local-climate-data-tool/Data/IntermediateData/Multi_Model_Stats_Arrays/'
 
-def get_scenario_fnames(data_path, scenario):
+def get_scenario_fnames(data_path, scenario, normalized=False):
     """
-    Get a string list of all zarr files in the data_path for the given 
+    Get a string list of all zarr files in the data_path for the given
     scenario. Prints the list to the user.
     """
     endcut = -1*len('.zarr')
     begcut = len(data_path)
-    names = [f[begcut:endcut] for f in glob.glob(data_path + '*_' + scenario + '_*.zarr')]
+    if normalized:
+        names = [f[begcut:endcut] for f in glob.glob(data_path +
+                                                     'Normalized*_' + scenario + '_*.zarr')]
+    else:
+        names = [f[begcut:endcut] for f in glob.glob(data_path +
+                                                     '*_' + scenario + '_*.zarr')]
     return names
 
 def read_in_fname(data_path, fname):
@@ -28,71 +34,71 @@ def read_in_fname(data_path, fname):
     filename = data_path + fname + '.zarr'
     return xr.open_zarr(filename)
 
-def initialize_empty_mms_arrays(data_path, initial_ds_fname, variable_name):
-    """Creates an empty numpy array with the correct dimensions based on
-    the shape of a single processed model data file"""
-    ds_init = read_in_fname(data_path, initial_ds_fname)
-    ds_init.load()
-    ds_dims = np.shape(ds_init[variable_name].values)
-    lats = ds_init['lat']
-    lons = ds_init['lon']
-    times = ds_init['time']
-    
-    empty_array = np.empty(ds_dims)
+def initialize_empty_mms_arrays(data_path, scenario_name, num_chunks,
+                                normalized=False):
+    """Add docstring"""
+    file_names = get_scenario_fnames(data_path, scenario_name, normalized)
+    datasets = [read_in_fname(data_path, x) for x in file_names]
 
-    mean_vals = np.copy(empty_array)
-    max_vals = np.copy(empty_array)
-    min_vals = np.copy(empty_array)
-    std_vals = np.copy(empty_array)
-    
-    return lats, lons, times, mean_vals, max_vals, min_vals, std_vals
+    nmodels = len(datasets)
+    times = datasets[0].time
+    lats = datasets[0]['lat']
+    lons = datasets[0]['lon']
+    dims = [lats, lons, times]
 
-def save_incomplete_dataset(output_path_temp, fname_temp, mean_vals, min_vals, max_vals, std_vals):
-    """Save multi-model mean numpy arrays as pickle files. This is useful
-    for saving partially-filled (incomplete) numpy arrays midway through analysis"""
-    with open(output_path_temp+fname_temp+'_mean'+'.pickle', 'wb') as handle:
-        pickle.dump(mean_vals, handle, protocol=pickle.HIGHEST_PROTOCOL)
+    ntime = len(times)
+    nlat = len(lats)
+    nlon = len(lons)
+    multi_model_means = np.empty((ntime, nlat, nlon))
+    multi_model_maxs = np.empty((ntime, nlat, nlon))
+    multi_model_mins = np.empty((ntime, nlat, nlon))
+    multi_model_stds = np.empty((ntime, nlat, nlon))
 
-    with open(output_path_temp+fname_temp+'_min'+'.pickle', 'wb') as handle:
-        pickle.dump(min_vals, handle, protocol=pickle.HIGHEST_PROTOCOL)
+    chunk_size = int(nlat/num_chunks)
 
-    with open(output_path_temp+fname_temp+'_max'+'.pickle', 'wb') as handle:
-        pickle.dump(max_vals, handle, protocol=pickle.HIGHEST_PROTOCOL)
+    boundary_cond = np.zeros(num_chunks)
+    boundary_cond[num_chunks-1] = -(chunk_size*num_chunks)+nlat
 
-    with open(output_path_temp+fname_temp+'_std'+'.pickle', 'wb') as handle:
-        pickle.dump(std_vals, handle, protocol=pickle.HIGHEST_PROTOCOL)
-        
-def fill_mms_arrays(scenario_name, lats, lons, mean_vals, min_vals, max_vals, std_vals,
-                    start_lat, end_lat):
-    """Fills empty numpy arrays with multi-model statics by iterating through
-    each grid cell and calculating multi-model stastics at each grid cell"""
-    file_names_hist = get_scenario_fnames(DATA_PATH, scenario_name)
-    datasets_hist = [read_in_fname(DATA_PATH, fname) for fname in file_names_hist]
-    start_time = time.time()
-    for i in range(start_lat, end_lat):
-        lt = lats.values[i]
-        print('calculating stats for '+str(i))
-        for j in range(0,len(lons.values)):
-            ln = lons.values[j]
-            stats_single_point = mms.export_stats(datasets_hist, file_names_hist, is_global_mean=False, coords=[lt, ln])
-            mean_vals[:,i,j] = stats_single_point['multi_mean'].values
-            min_vals[:,i,j] = stats_single_point['multi_min'].values
-            max_vals[:,i,j] = stats_single_point['multi_max'].values
-            std_vals[:,i,j] = stats_single_point['multi_std'].values
-        end_time = time.time()
-        print(str(i)+' complete')
-        print(end_time - start_time)
-        
-        # Save temporary files every 10 latitude bands
-        if np.mod(i,10)==0:
-            save_incomplete_dataset(output_path_temp=INTERMEDIATE_OUTPUT_PATH,
-                                    fname_temp='temporary_multi_model_stats_grid_'+scenario_name,
-                                    mean_vals=mean_vals,
-                                    min_vals=min_vals,
-                                    max_vals=max_vals,
-                                    std_vals=std_vals)
+    nlat0_chunk = chunk_size*np.arange(0, num_chunks)
+    nlatf_chunk = chunk_size*np.arange(1, num_chunks+1) +boundary_cond
 
-    return mean_vals, min_vals, max_vals, std_vals
+    empty_dsets = [multi_model_means, multi_model_mins, multi_model_maxs, multi_model_stds]
+    dim_info = [nlat0_chunk, nlatf_chunk, nmodels, ntime, nlat, nlon]
+
+    return [empty_dsets, dim_info, dims, file_names, datasets]
+
+def fill_empty_arrays(empty_dsets, dim_info, file_names, datasets, varname, num_chunks):
+    """Add docstring"""
+    [multi_model_means,
+     multi_model_mins,
+     multi_model_maxs,
+     multi_model_stds] = empty_dsets
+    [nlat0_chunk, nlatf_chunk, nmodels, ntime, _, nlon] = dim_info
+    for c in range(0, num_chunks):
+        # Define bounds for data chunk
+        nlat0 = int(nlat0_chunk[c])
+        nlatf = int(nlatf_chunk[c])
+        nlat_chunk = nlatf - nlat0
+
+        # Get data chunk from all models
+        model_data_chunk = np.empty((nmodels, ntime, nlat_chunk, nlon))
+        for i in range(nmodels):
+            #model_name = file_names[i].split("_")[2]
+            #print(model_name)
+            model_data_chunk[i, :, :, :] = datasets[i][varname][:, nlat0:nlatf, :]
+
+        multi_mean_chunk = np.nanmean(model_data_chunk, axis=0)
+        multi_min_chunk = np.nanmin(model_data_chunk, axis=0)
+        multi_max_chunk = np.nanmax(model_data_chunk, axis=0)
+        multi_std_chunk = np.nanstd(model_data_chunk, axis=0)
+
+        multi_model_means[:, nlat0:nlatf, :] = multi_mean_chunk
+        multi_model_mins[:, nlat0:nlatf, :] = multi_min_chunk
+        multi_model_maxs[:, nlat0:nlatf, :] = multi_max_chunk
+        multi_model_stds[:, nlat0:nlatf, :] = multi_std_chunk
+
+    return [multi_model_means, multi_model_mins,
+            multi_model_maxs, multi_model_stds]
 
 def create_xr_dataset(lats, lons, times, mean_vals, max_vals, min_vals, std_vals):
     """Creates an xarray dataset from numpy arrays of multi-model statics"""
@@ -103,7 +109,7 @@ def create_xr_dataset(lats, lons, times, mean_vals, max_vals, min_vals, std_vals
     max_xr = xr.DataArray(max_vals, dims=ds_dims, coords=ds_coords)
     min_xr = xr.DataArray(min_vals, dims=ds_dims, coords=ds_coords)
     std_xr = xr.DataArray(std_vals, dims=ds_dims, coords=ds_coords)
-    
+
     # Create xarray dataset from xarrays
     ds = xr.Dataset({'mean': mean_xr,
                      'min': min_xr,
@@ -113,38 +119,72 @@ def create_xr_dataset(lats, lons, times, mean_vals, max_vals, min_vals, std_vals
 
     return ds
 
-def export_dataset(ds, output_path, variable_name, scenario_name):
+def export_dataset(ds, output_path, variable_name, scenario_name, normalized=False):
     """Exports dataset to zarr file"""
     ds.chunk({'lon':10, 'lat':10, 'time':-1})
-    ds.to_zarr(output_path+'modelData_'+variable_name+'_'+scenario_name+'.zarr')
-    
-def create_scenario_mms_datasets(data_path,
-                                 variable_name,
-                                 scenario_name,
-                                 modelname_init='UKESM1-0-LL'):
-    
-    initial_ds_fname = variable_name+'_'+scenario_name+'_'+modelname_init
-    [lats, lons, times,
-     mean_vals, max_vals,
-     min_vals, std_vals] = initialize_empty_mms_arrays(data_path,
-                                                       initial_ds_fname,
-                                                       variable_name)
-    
-    [mean_vals, min_vals,
-     max_vals, std_vals] = fill_mms_arrays(scenario_name=scenario_name,
-                                           lats=lats, lons=lons,
-                                           mean_vals=mean_vals, min_vals=min_vals,
-                                           max_vals=max_vals, std_vals=std_vals,
-                                           start_lat=0, end_lat=len(lats.values))
+    if normalized:
+        ds.to_zarr(output_path+'modelData_normalized_'+
+                   variable_name+'_'+scenario_name+'.zarr')
+    else:
+        ds.to_zarr(output_path+'modelData_'+
+                   variable_name+'_'+scenario_name+'.zarr')
 
-    
+def create_scenario_mms_datasets(variable_name,
+                                 scenario_name,
+                                 num_chunks,
+                                 data_path=DATA_PATH,
+                                 normalized=False):
+    """Add docstring"""
+
+    print('Creating empty arrays')
+    [empty_dsets,
+     dim_info,
+     dims,
+     file_names,
+     datasets] = initialize_empty_mms_arrays(data_path,
+                                             scenario_name=scenario_name,
+                                             num_chunks=20,
+                                             normalized=normalized)
+    [lats, lons, times] = dims
+
+    print('Calculating multimodel statistics')
+    [mean_vals,
+     min_vals,
+     max_vals,
+     std_vals] = fill_empty_arrays(empty_dsets,
+                                   dim_info,
+                                   file_names,
+                                   datasets,
+                                   variable_name,
+                                   num_chunks)
+
+
+    print('Exporting dataset')
     ds = create_xr_dataset(lats, lons, times, mean_vals, max_vals, min_vals, std_vals)
-    
+
     export_dataset(ds=ds,
                    output_path=OUTPUT_PATH,
                    variable_name=variable_name,
-                   scenario_name=scenario_name)
-    
+                   scenario_name=scenario_name,
+                   normalized=normalized)
+
     return lats, lons, times, mean_vals, max_vals, min_vals, std_vals
-    
-    
+
+
+#------------------MAIN WORKFLOW----------------------------------------
+for scenario_name in SCENARIO_LIST:
+    print('-----------'+scenario_name+'-----------')
+    start_time = time.time()
+    [lats,
+     lons,
+     times,
+     mean_vals,
+     max_vals,
+     min_vals,
+     std_vals] = create_scenario_mms_datasets(data_path=DATA_PATH,
+                                              variable_name=VARIABLE_NAME,
+                                              scenario_name=scenario_name,
+                                              num_chunks=20,
+                                              normalized=True)
+    end_time = time.time()
+    print(end_time - start_time)
