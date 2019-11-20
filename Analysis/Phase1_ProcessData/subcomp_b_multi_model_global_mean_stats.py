edcb33e20b3cb953489cde70c3ad2288d147779c
Default user <jnug@uw.edu>
2019-11-20 14:48:37 +0000

    Changed to calculate stats and make files in one script

---
+++ Analysis/Phase1_ProcessData/subcomp_b_multi_model_global_mean_stats.py
@@ -0,0 +1,150 @@
+"""
+Module to generate datasets of multimodel statistics for each of the CMIP6
+scenarios for the global mean data (dimension time).
+
+Make sure to clear the directory you would like the zarr files to be saved
+in - something about this format makes it not work to overwrite existing
+files.
+
+Author: Jacqueline Nugent
+Last Modified: November 20, 2019
+"""
+import glob
+import xarray as xr
+import numpy as np
+
+import analysis_parameters
+
+
+DATA_DIR = analysis_parameters.DIR_INTERMEDIATE_PROCESSED_GLOBAL_DATA
+OUT_DIR = analysis_parameters.DIR_PROCESSED_DATA + 'model_data/global_mean_data/'
+SCENARIOS = analysis_parameters.EXPERIMENT_LIST
+
+
+def read_zarr_files(data_path):
+    """
+    Open all <fname>.zarr files in the data_path. Returns a dictionary with
+    key scenario name and value list of datasets in the scenario.
+    """
+    # open zarr files & get list of datasets for each scenario
+    nexp = len(SCENARIOS)
+    ds_list = [[] for i in range(nexp)]
+    for s in range(nexp):
+        sname = SCENARIOS[s]
+        files = [f for f in glob.glob(data_path + '*_' + sname + '_*.zarr')]
+        datasets = [xr.open_zarr(f) for f in files]
+        ds_list[s] = datasets
+
+    # make the dictionary
+    data_dict = dict(zip(SCENARIOS, ds_list))
+
+    return data_dict
+
+
+def compute_stats(datasets, file_names):
+    """
+    Compute the mean, minimum, maximum, and standard deviation values of one
+    variable across the global means of each model.
+
+    Args:
+        datasets, list of the processed global mean Datasets in one scenario
+        file_names, list of the names of the zarr files containing the
+                    datasets in the form
+                    "VARIABLE_SCENARIO_MODEL_GLOBALMEAN.zarr"
+    Returns:
+        stats_list, a list of DataArrays containing the computed statistics
+                    in the form [multi_mean, multi_max, multi_min, multi_std].
+    """
+    times = datasets[0].time
+    varname = file_names[0].split("_")[0]
+    model_global_means = np.empty((len(datasets), len(times)))
+
+    ### loop through models to select global mean at each time step
+    for i in range(len(datasets)):
+        model_global_means[i, :] = datasets[i][varname]
+
+    ### calculate statistics
+    multi_mean_calc = np.nanmean(model_global_means, axis=0)
+    multi_min_calc = np.nanmin(model_global_means, axis=0)
+    multi_max_calc = np.nanmax(model_global_means, axis=0)
+    multi_std_calc = np.nanstd(model_global_means, axis=0)
+
+    ### create the DataArrays
+    multi_mean = xr.DataArray(data=multi_mean_calc, coords={'time': times}, dims='time')
+    multi_min = xr.DataArray(data=multi_min_calc, coords={'time': times}, dims='time')
+    multi_max = xr.DataArray(data=multi_max_calc, coords={'time': times}, dims='time')
+    multi_std = xr.DataArray(data=multi_std_calc, coords={'time': times}, dims='time')
+
+    stats_list = [multi_mean, multi_min, multi_max, multi_std]
+
+    return stats_list
+
+
+def export_stats(datasets, file_names):
+    """
+    Exports the computed multimodel statistics to a Dataset.
+
+    as Dataset with dimension time and variables multi_mean, multi_min, multi_max,
+    and multi_std. Scenario name and variable name (e.g. 'tas') are attributes
+    of the dataset.
+
+    Args:
+        datasets, list of the processed global mean Datasets in one scenario
+        file_names, list of the names of the zarr files containing the
+                    datasets in the form
+                    "VARIABLE_SCENARIO_MODEL_GLOBALMEAN.zarr"
+    Returns:
+        multi_stats, a Dataset with dimensions latitude, longitude, and time;
+                    variables multi_mean, multi_min, multi_max, and multi_std;
+                    and attributes variable (name of the variable, e.g. 'tas')
+                    and scenario (name of the scenario, e.g. 'historical').
+    Exceptions:
+        ValueError, raised if datasets and file_names have different lengths
+    """
+    ### check that datasets and file_names have the same length
+    check = len(datasets) == len(file_names)
+    if not check:
+        raise ValueError('Length of file_names must be the same as length of'
+                         'datasets.')
+
+    ### get the name of the scenario
+    this_scenario = file_names[0].split('_')[1]
+    this_var = file_names[0].split('_')[0]
+
+    ### calculate statistics
+    [mm_mean, mm_min, mm_max, mm_std] = compute_statistics(datasets, file_names)
+
+    ### make dictionary for the data
+    varnames = ['multi_mean', 'multi_min', 'multi_max', 'multi_std']
+    stats = [mm_mean, mm_min, mm_max, mm_std]
+    stats_dict = dict(zip(varnames, stats))
+
+    ### export it into a single Dataset
+    multi_stats = xr.Dataset(data_vars=stats_dict,
+                             attrs={'variable': this_var,
+                                    'scenario': this_scenario})
+
+    return multi_stats
+
+
+def save_dataset(ds, out_path):
+    """Save the multimodel statistics dataset as a zarr file."""
+    ds.load()
+    file_name = ds.variable + '_' + ds.scenario + '_GLOBALMEAN_STATS'
+    ds.chunk({'time':10})
+    ds.to_zarr(out_path + file_name + '.zarr')
+
+
+####### MAIN WORKFLOW ########
+
+GLOBAL_MEAN_DICT = read_zarr_files(DATA_DIR)
+
+### for global mean files:
+for key in GLOBAL_MEAN_DICT.keys():
+    # compute the global mean multimodel stats
+    dataset_list = GLOBAL_MEAN_DICT[key]
+    fnames = [dataset.file_name for dataset in dataset_list]
+    ds_gm_stats = export_stats(dataset_list, fnames)
+
+    # save the global mean multitmodel stats dataset
+    save_dataset(ds_gm_stats, OUT_DIR)
