d11bca35958a6d6d6d89a6eb7806bbd3952b753e
İhsan Kahveci <ihsankahveci@Ihsans-MacBook-Pro.local>
2019-11-21 01:57:29 -0800

    added download_url

--- setup.py
+++ setup.py
@@ -15,17 +15,19 @@ SETUP_REQUIRES = ['setuptools >= 24.2.0']
 opts = dict(name='Local-Climate-Data-Tool',
             maintainer='Claire Zarakas',
             maintainer_email='czarakas@uw.edu',
-            description='"""
-            This local climate data tool displays the data from CMIP-6 output for historical
-            and 4 different future scenarios via the Shared Socioeconomic Pathways (SSPs)
-            described below. The motivation for this tool is to make climate data more
-            accessible to people with little to no computing knowledge or background in
-            atmospheric science or climate studies.
+            description=
+            '"""
+            This local climate data tool displays the data from CMIP-6
+            output for historical and 4 different future scenarios via
+            the Shared Socioeconomic Pathways (SSPs) described below.
+            The motivation for this tool is to make climate data more
+            accessible to people with little to no computing knowledge or
+            background in atmospheric science or climate studies.
             """,
             long_description=open('README.md').read(),
             url='https://github.com/czarakas/local-climate-data-tool',
-            # download_url='https://github.com/czarakas/local-climate-data-tool.git',
-            license='LICENSE',
+            download_url='https://github.com/czarakas/local-climate-data-tool/archive/master.zip',
+            license='MIT Licence',
             # classifiers="CLASSIFIERS",
             # author='Ax/Wx"',
             # author_email='axwx@googlegroups.com',
