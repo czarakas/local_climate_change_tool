89b8f77b9462b8ab6abfad12e850424ceef89788
İhsan Kahveci <ihsankahveci@Ihsans-MacBook-Pro.local>
2019-11-30 13:21:06 -0800

    name corrected

---
+++ dummy_test.py
@@ -0,0 +1,10 @@
+import unittest
+
+
+class TestSum(unittest.TestCase):
+
+    def test_sum(self):
+        self.assertEqual(sum([1, 2, 3]), 6, "Should be 6")
+
+if __name__ == '__main__':
+    unittest.main()
