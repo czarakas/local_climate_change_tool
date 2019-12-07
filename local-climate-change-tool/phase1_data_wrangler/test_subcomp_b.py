4ce3546369120955555953d79534fb2fa6ebb7d2
czarakas <czarakas@uw.edu>
2019-12-07 23:46:35 +0000

    Changed testing files to use unittest

--- local-climate-change-tool/phase1_data_wrangler/test_subcomp_b.py
+++ local-climate-change-tool/phase1_data_wrangler/test_subcomp_b.py
@@ -5,6 +5,7 @@ Tests for subcomponent b
 # Import modules
 import datetime
 import cftime
+import unittest
 import os
 import glob
 import sys
@@ -39,167 +40,6 @@ DSET_DICT[TEST_KEY2] = xr.open_dataset(TESTING_DATA_DIR+TEST_KEY2+'.nc')
 
 #---------------------------------------------------------------------------------
 #---------------------------------------------------------------------------------
-def test_reindex_time():
-    """Tests whether the reindex_time function works as expected"""
-    [yr, month, day] = [1850, 1, 15]
-    expected_times =xr.DataArray(np.array([cftime.DatetimeProlepticGregorian(yr, month, day, 0, 0),
-                                           cftime.DatetimeProlepticGregorian(yr, month+1, day, 0, 0)]))
-    testdates1 = xr.DataArray(np.array([np.datetime64(datetime.datetime(yr, month, day, 0, 0)),
-                                        np.datetime64(datetime.datetime(yr, month+1, day, 0, 0))]))
-    testdates2 = xr.DataArray(np.array([cftime.DatetimeNoLeap(yr, month, day, 0, 0),
-                                        cftime.DatetimeNoLeap(yr, month+1, day, 0, 0)]))
-    testdates3 = xr.DataArray(np.array([cftime.Datetime360Day(yr, month, day, 0, 0),
-                                        cftime.Datetime360Day(yr, month+1, day, 0, 0)]))
-    dates_to_test = [testdates1, testdates2, testdates3]
-
-    # Run test
-    dates_converted = True
-    for date_to_test in dates_to_test:
-        newtimes = process_data.reindex_time(date_to_test)
-        if not np.all((newtimes==expected_times).values):
-            dates_converted = False
-        else:
-            continue
-    assert dates_converted
-
-def test_generate_new_filename(test_key=TEST_KEY1):
-    """Test that the generate_new_filename function generates a string"""
-    fname = process_data.generate_new_filename(test_key)
-    assert ((fname is not None) and (type(fname) == str))
-
-def test_create_reference_grid(dset_dict=DSET_DICT, test_key=TEST_KEY2):
-    """ Tests that the reference grid function creates a sensible reference grid"""
-    ds_original = dset_dict[test_key]
-    ds_ref_grid = process_data.create_reference_grid(dset_dict=dset_dict,
-                                                     reference_key=test_key)
-
-    # Check that latitude and longitude dimensions haven't changed in regridding
-    correct_dim_size = ((ds_original.dims['lat'] == ds_ref_grid.dims['lat']) and
-                        (ds_original.dims['lon'] == ds_ref_grid.dims['lon']))
-
-    # Check that reference grid doesn't have any variables other than lat and lon
-    no_extra_vars = (len(ds_ref_grid.variables) == 2)
-
-    # Check that latitude and longitude coordinates do not contain nans
-    no_nans = ((not np.isnan(ds_ref_grid['lat'].values).any()) and
-               (not np.isnan(ds_ref_grid['lon'].values).any()))
-
-    #Check that latitude and longitude arrays are 1D
-    one_dim = ((np.size(ds_ref_grid['lat'].values) == np.shape(ds_ref_grid['lat'].values)[0]) and
-               (np.size(ds_ref_grid['lon'].values) == np.shape(ds_ref_grid['lon'].values)[0]))
-
-    # Check that latitude and longitude arrays are big enough
-    min_coord_size = 20
-    big_enough = ((np.size(ds_ref_grid['lat'].values) > min_coord_size) and
-                  (np.size(ds_ref_grid['lon'].values) > min_coord_size))
-
-    assert correct_dim_size
-    assert no_extra_vars
-    assert no_nans
-    assert one_dim
-    assert big_enough
-
-def test_regrid_model_dims(dset_dict=DSET_DICT, key_to_regrid=TEST_KEY1,
-                           key_for_grid=TEST_KEY2):
-    """Tests that the regrid function results in an array of the right dimensions"""
-    
-    # Regrid model output
-    ds_to_regrid = dset_dict[key_to_regrid]
-    reference_grid = process_data.create_reference_grid(dset_dict=dset_dict,
-                                                        reference_key=key_for_grid)
-    ds_regridded = process_data.regrid_model(ds_to_regrid,
-                                             reference_grid)
-    
-    # Check that lat/lonregridded data actually aligns with reference grid
-    correct_lat_dim = np.size(ds_regridded['lat'].values) == np.size(reference_grid['lat'].values)
-    correct_lon_dim = np.size(ds_regridded['lon'].values) == np.size(reference_grid['lon'].values)
-    # Check that time dimensions have not changed
-    correct_time_dim = np.equal(ds_regridded['time'].values,ds_to_regrid['time'].values).all()
-    assert (correct_lat_dim and correct_lon_dim and correct_time_dim)
-
-def test_regrid_model_nans(dset_dict=DSET_DICT, key_to_regrid=TEST_KEY1,
-                           key_for_grid=TEST_KEY2, varname=VARNAME):
-    """Tests that the regrid function doesn't create any nans"""
-    ds_to_regrid = dset_dict[key_to_regrid]
-    reference_grid = process_data.create_reference_grid(dset_dict=dset_dict,
-                                                        reference_key=key_for_grid)
-    ds_regridded = process_data.regrid_model(ds_to_regrid,
-                                             reference_grid)
-
-    # Check that there aren't nans if there weren't in original array
-    if np.isnan(ds_to_regrid[varname].values).any():
-        raise ValueError('NANs were in original array')
-    else: 
-        assert not np.isnan(ds_regridded[varname].values).any()
-
-def test_process_dataset(dset_dict=DSET_DICT,
-                         key_to_process=TEST_KEY1,
-                         key_for_grid=TEST_KEY2,
-                         test_inds=TEST_INDS,
-                         expected_types=EXP_TYPES):
-    """Tests whether the processed dataset output by the process_dataset
-    function has expected coordinates and dimensions and plausible values"""
-    #------------------ Process test dataset------------------------------------
-    ds_original = dset_dict[key_to_process]
-    ds_ref_grid = process_data.create_reference_grid(dset_dict=dset_dict,
-                                                     reference_key=key_for_grid)
-    ds_processed = process_data.process_dataset(this_key=key_to_process,
-                                                dset_dict=dset_dict,
-                                                final_grid=ds_ref_grid)
-
-    #------------------ Run tests on processed dataset-------------------------
-    # Check that lat/lon/time coordinate names are right
-    coord_names_pass = check_coord_names(ds_processed, ds_coords_expected=['time', 'lon', 'lat'])
-
-    # Check that time is reasonable (e.g. )
-    years_pass = check_years(ds_processed, min_year=1849, max_year=2200)
-
-    # Check that lat, lon, and time dimensions are what we expect
-    dims_pass = check_dims(ds_processed, ds_original, ds_ref_grid)
-
-    # Check that coordinate types are right
-    coord_types_pass = check_coord_types(ds_processed, expected_types)
-
-    assert coord_names_pass
-    assert years_pass
-    assert dims_pass
-    assert coord_types_pass
-
-def test_save_dataset(dset_dict=DSET_DICT,
-                      data_path_out=TESTING_OUTPUT_DIR,
-                      key_for_grid=TEST_KEY2,
-                      exceptions_list=(),
-                     expected_types=EXP_TYPES):
-    final_grid = process_data.create_reference_grid(dset_dict=dset_dict,
-                                                    reference_key=key_for_grid)
-    
-    delete_zarr_files(data_dir=data_path_out, regex='*')
-    
-    process_data.process_all_files_in_dictionary(dset_dict, exceptions_list,
-                                        final_grid, data_path_out)
-    endcut = -1*len('.zarr')
-    begcut = len(data_path_out)
-    names = [f[begcut:endcut] for f in glob.glob(data_path_out +'*.zarr')]
-    files_are_saved = bool(len(names) > 0)
-    
-    coord_names_pass = True
-    years_pass = True
-    coord_types_pass = True
-    for fname in names:
-        filename = data_path_out + fname + '.zarr'
-        ds = xr.open_zarr(filename)
-        ds = xr.decode_cf(ds)
-        
-        # Check that ds looks the same as the processed dataset
-        coord_names_pass = coord_names_pass and check_coord_names(ds, ds_coords_expected=['time', 'lon', 'lat'])
-        coord_types_pass = coord_types_pass and check_coord_types(ds, expected_types)
-        years_pass = years_pass and check_years(ds, min_year=1849, max_year=2200)
-    
-    assert files_are_saved
-    assert coord_names_pass
-    assert years_pass
-    assert coord_types_pass
-
 def check_coord_names(ds_processed, ds_coords_expected):
     """Checks whether coordinate names of ds are expected names"""
     coords_list = []
@@ -209,7 +49,7 @@ def check_coord_names(ds_processed, ds_coords_expected):
 
 def check_dims(ds_processed, ds_original, ds_ref_grid):
     """Checks whether dimensions of dataset are expected based on
-    regridding and original dataset dimensions"""
+     regridding and original dataset dimensions"""
     if ds_processed.dims['time'] == ds_original.dims['time']:
         if  ds_processed.dims['lat'] == ds_ref_grid.dims['lat']:
             if ds_processed.dims['lon'] == ds_ref_grid.dims['lon']:
@@ -265,3 +105,170 @@ def delete_zarr_files(data_dir, regex):
         os.system('rm -rf '+file)
         i = i+1
     print('deleted '+str(i)+' files in '+data_dir)
+    
+class test_subcomp_a(unittest.TestCase):
+    def test_reindex_time(self):
+        """Tests whether the reindex_time function works as expected"""
+        [yr, month, day] = [1850, 1, 15]
+        expected_times =xr.DataArray(np.array([cftime.DatetimeProlepticGregorian(yr, month, day, 0, 0),
+                                               cftime.DatetimeProlepticGregorian(yr, month+1, day, 0, 0)]))
+        testdates1 = xr.DataArray(np.array([np.datetime64(datetime.datetime(yr, month, day, 0, 0)),
+                                            np.datetime64(datetime.datetime(yr, month+1, day, 0, 0))]))
+        testdates2 = xr.DataArray(np.array([cftime.DatetimeNoLeap(yr, month, day, 0, 0),
+                                            cftime.DatetimeNoLeap(yr, month+1, day, 0, 0)]))
+        testdates3 = xr.DataArray(np.array([cftime.Datetime360Day(yr, month, day, 0, 0),
+                                            cftime.Datetime360Day(yr, month+1, day, 0, 0)]))
+        dates_to_test = [testdates1, testdates2, testdates3]
+
+        # Run test
+        dates_converted = True
+        for date_to_test in dates_to_test:
+            newtimes = process_data.reindex_time(date_to_test)
+            if not np.all((newtimes==expected_times).values):
+                dates_converted = False
+            else:
+                continue
+        self.assertTrue(dates_converted)
+
+    def test_generate_new_filename(self, test_key=TEST_KEY1):
+        """Test that the generate_new_filename function generates a string"""
+        fname = process_data.generate_new_filename(test_key)
+        self.assertTrue(((fname is not None) and (type(fname) == str)))
+
+    def test_create_reference_grid(self, dset_dict=DSET_DICT, test_key=TEST_KEY2):
+        """ Tests that the reference grid function creates a sensible reference grid"""
+        ds_original = dset_dict[test_key]
+        ds_ref_grid = process_data.create_reference_grid(dset_dict=dset_dict,
+                                                         reference_key=test_key)
+
+        # Check that latitude and longitude dimensions haven't changed in regridding
+        correct_dim_size = ((ds_original.dims['lat'] == ds_ref_grid.dims['lat']) and
+                            (ds_original.dims['lon'] == ds_ref_grid.dims['lon']))
+
+        # Check that reference grid doesn't have any variables other than lat and lon
+        no_extra_vars = (len(ds_ref_grid.variables) == 2)
+
+        # Check that latitude and longitude coordinates do not contain nans
+        no_nans = ((not np.isnan(ds_ref_grid['lat'].values).any()) and
+                   (not np.isnan(ds_ref_grid['lon'].values).any()))
+
+        #Check that latitude and longitude arrays are 1D
+        one_dim = ((np.size(ds_ref_grid['lat'].values) == np.shape(ds_ref_grid['lat'].values)[0]) and
+                   (np.size(ds_ref_grid['lon'].values) == np.shape(ds_ref_grid['lon'].values)[0]))
+
+        # Check that latitude and longitude arrays are big enough
+        min_coord_size = 20
+        big_enough = ((np.size(ds_ref_grid['lat'].values) > min_coord_size) and
+                      (np.size(ds_ref_grid['lon'].values) > min_coord_size))
+
+        self.assertTrue(correct_dim_size)
+        self.assertTrue(no_extra_vars)
+        self.assertTrue(no_nans)
+        self.assertTrue(one_dim)
+        self.assertTrue(big_enough)
+
+    def test_regrid_model_dims(self, dset_dict=DSET_DICT, key_to_regrid=TEST_KEY1,
+                               key_for_grid=TEST_KEY2):
+        """Tests that the regrid function results in an array of the right dimensions"""
+
+        # Regrid model output
+        ds_to_regrid = dset_dict[key_to_regrid]
+        reference_grid = process_data.create_reference_grid(dset_dict=dset_dict,
+                                                            reference_key=key_for_grid)
+        ds_regridded = process_data.regrid_model(ds_to_regrid,
+                                                 reference_grid)
+
+        # Check that lat/lonregridded data actually aligns with reference grid
+        correct_lat_dim = np.size(ds_regridded['lat'].values) == np.size(reference_grid['lat'].values)
+        correct_lon_dim = np.size(ds_regridded['lon'].values) == np.size(reference_grid['lon'].values)
+        # Check that time dimensions have not changed
+        correct_time_dim = np.equal(ds_regridded['time'].values,ds_to_regrid['time'].values).all()
+        self.assertTrue(correct_lat_dim and correct_lon_dim and correct_time_dim)
+
+    def test_regrid_model_nans(self, dset_dict=DSET_DICT, key_to_regrid=TEST_KEY1,
+                               key_for_grid=TEST_KEY2, varname=VARNAME):
+        """Tests that the regrid function doesn't create any nans"""
+        ds_to_regrid = dset_dict[key_to_regrid]
+        reference_grid = process_data.create_reference_grid(dset_dict=dset_dict,
+                                                            reference_key=key_for_grid)
+        ds_regridded = process_data.regrid_model(ds_to_regrid,
+                                                 reference_grid)
+
+        # Check that there aren't nans if there weren't in original array
+        if np.isnan(ds_to_regrid[varname].values).any():
+            raise ValueError('NANs were in original array')
+        else: 
+            self.assertTrue(not np.isnan(ds_regridded[varname].values).any())
+
+    def test_process_dataset(self,
+                             dset_dict=DSET_DICT,
+                             key_to_process=TEST_KEY1,
+                             key_for_grid=TEST_KEY2,
+                             test_inds=TEST_INDS,
+                             expected_types=EXP_TYPES):
+        """Tests whether the processed dataset output by the process_dataset
+        function has expected coordinates and dimensions and plausible values"""
+        #------------------ Process test dataset------------------------------------
+        ds_original = dset_dict[key_to_process]
+        ds_ref_grid = process_data.create_reference_grid(dset_dict=dset_dict,
+                                                         reference_key=key_for_grid)
+        ds_processed = process_data.process_dataset(this_key=key_to_process,
+                                                    dset_dict=dset_dict,
+                                                    final_grid=ds_ref_grid)
+
+        #------------------ Run tests on processed dataset-------------------------
+        # Check that lat/lon/time coordinate names are right
+        coord_names_pass = check_coord_names(ds_processed, ds_coords_expected=['time', 'lon', 'lat'])
+
+        # Check that time is reasonable (e.g. )
+        years_pass = check_years(ds_processed, min_year=1849, max_year=2200)
+
+        # Check that lat, lon, and time dimensions are what we expect
+        dims_pass = check_dims(ds_processed, ds_original, ds_ref_grid)
+
+        # Check that coordinate types are right
+        coord_types_pass = check_coord_types(ds_processed, expected_types)
+
+        self.assertTrue( coord_names_pass)
+        self.assertTrue( years_pass)
+        self.assertTrue( dims_pass)
+        self.assertTrue( coord_types_pass)
+
+    def test_save_dataset(self,
+                          dset_dict=DSET_DICT,
+                          data_path_out=TESTING_OUTPUT_DIR,
+                          key_for_grid=TEST_KEY2,
+                          exceptions_list=(),
+                         expected_types=EXP_TYPES):
+        final_grid = process_data.create_reference_grid(dset_dict=dset_dict,
+                                                        reference_key=key_for_grid)
+
+        delete_zarr_files(data_dir=data_path_out, regex='*')
+
+        process_data.process_all_files_in_dictionary(dset_dict, exceptions_list,
+                                            final_grid, data_path_out)
+        endcut = -1*len('.zarr')
+        begcut = len(data_path_out)
+        names = [f[begcut:endcut] for f in glob.glob(data_path_out +'*.zarr')]
+        files_are_saved = bool(len(names) > 0)
+
+        coord_names_pass = True
+        years_pass = True
+        coord_types_pass = True
+        for fname in names:
+            filename = data_path_out + fname + '.zarr'
+            ds = xr.open_zarr(filename)
+            ds = xr.decode_cf(ds)
+
+            # Check that ds looks the same as the processed dataset
+            coord_names_pass = coord_names_pass and check_coord_names(ds, ds_coords_expected=['time', 'lon', 'lat'])
+            coord_types_pass = coord_types_pass and check_coord_types(ds, expected_types)
+            years_pass = years_pass and check_years(ds, min_year=1849, max_year=2200)
+
+        self.assertTrue( files_are_saved)
+        self.assertTrue( coord_names_pass)
+        self.assertTrue( years_pass)
+        self.assertTrue( coord_types_pass)
+        
+if __name__ == '__main__':
+    unittest.main()
--- local-climate-change-tool/phase1_data_wrangler/test_subcomp_d.py
+++ local-climate-change-tool/phase1_data_wrangler/test_subcomp_d.py
@@ -9,6 +9,7 @@ import xarray as xr
 import pandas as pd
 import numpy as np
 import glob
+import unittest
 
 sys.path.append(".")
 
@@ -27,101 +28,92 @@ OUTPUT_FILE = TEST_OUTPUT_DIR + OUTPUT_FILE_NAME
 EXP_TYPES = np.array([xr.core.dataset.Dataset, np.float32, np.float64, np.datetime64])
 EXP_DIMS = [180, 360, 2037]
 
+class test_subcomp_d(unittest.TestCase):
+    def test_convert_to_360(self):
+        """
+        Tests if the convert_to_360 function correctly converts -180 to 180 longitudes to
+        0 to 360 longitudes.
+        """
+        test_lons = [-180., 180., 0., 360., -135., 135.]
+        exp_lons = [180., 180., 0., 360., 225., 135.]
 
-def test_convert_to_360():
-    """
-    Tests if the convert_to_360 function correctly converts -180 to 180 longitudes to
-    0 to 360 longitudes.
-    """
-    test_lons = [-180., 180., 0., 360., -135., 135.]
-    exp_lons = [180., 180., 0., 360., 225., 135.]
-    
-    conv_lons = process_obs.convert_to_360(test_lons)
-    
-    assert np.array_equal(exp_lons, conv_lons)
+        conv_lons = process_obs.convert_to_360(test_lons)
 
-
-def test_calculate_temps():
-    """
-    Tests that the raw observations file exists and calculate_temps function returns a list 
-    of length 4 with t_avg, time, and lat and lon with the expected data types.
-    """
-    output = process_obs.calculate_temps(filename=TEST_FILE)
-    exp_length = 4
-    exp_types = [np.ndarray, pd.core.indexes.datetimes.DatetimeIndex, 
-                      np.ndarray, np.ndarray]
-    
-    correct_length = len(output) == exp_length
-    if correct_length:
-        correct_types = all([isinstance(output[i], exp_types[i]) for i in range(exp_length)])
-    else:
-        correct_types = False
-        
-    assert correct_types
-    assert correct_length
+        self.assertTrue(np.array_equal(exp_lons, conv_lons))
 
 
-def test_create_obs_dataset():
-    """
-    Tests that the create_obs_dataset function returns a dataset with the expected types and
-    dimensions.
-    """
-    [t_avg, time, lat, lon] = process_obs.calculate_temps(filename=TEST_FILE)
-    test_ds = process_obs.create_obs_dataset(t_avg, time, lat, lon)
-    test_coords = [x for x in test_ds.coords]
-    
-    ds_pass = isinstance(test_ds, EXP_TYPES[0])
-    lat_pass = (isinstance(test_ds['lat'].values[0], EXP_TYPES[1]) and len(test_ds['lat'].values) == EXP_DIMS[0])
-    lon_pass = (isinstance(test_ds['lon'].values[0], EXP_TYPES[2]) and len(test_ds['lon'].values) == EXP_DIMS[1])
-    time_pass = (isinstance(test_ds['time'].values[0], EXP_TYPES[3]) and len(test_ds['time'].values) == EXP_DIMS[2])
+    def test_calculate_temps(self):
+        """
+        Tests that the raw observations file exists and calculate_temps function returns a list 
+        of length 4 with t_avg, time, and lat and lon with the expected data types.
+        """
+        output = process_obs.calculate_temps(filename=TEST_FILE)
+        exp_length = 4
+        exp_types = [np.ndarray, pd.core.indexes.datetimes.DatetimeIndex, 
+                          np.ndarray, np.ndarray]
 
-    assert ds_pass
-    assert lat_pass
-    assert lon_pass
-    assert time_pass
+        correct_length = len(output) == exp_length
+        if correct_length:
+            correct_types = all([isinstance(output[i], exp_types[i]) for i in range(exp_length)])
+        else:
+            correct_types = False
+
+        self.assertTrue(correct_types)
+        self.assertTrue(correct_length)
 
 
-def test_save_dataset(): 
-    """
-    Tests that the dataset was saved correctly and looks the same as the processed dataset.
-    """
-    # first delete the file if it already exists in the directory - zarr can't overwrite
-    dw.delete_zarr_files(data_dir=TEST_OUTPUT_DIR, regex=OUTPUT_FILE_NAME, file_type='')
-
-    process_obs.process_all_observations(data_path=TEST_DATA_DIR,
-                                        data_path_out=TEST_OUTPUT_DIR)
-
-    out_dir_files = glob.glob(TEST_OUTPUT_DIR + '*.zarr', recursive=True)
-    file_exists = OUTPUT_FILE in out_dir_files
-
-    if file_exists:
-        test_ds = xr.open_zarr(OUTPUT_FILE)
+    def test_create_obs_dataset(self):
+        """
+        Tests that the create_obs_dataset function returns a dataset with the expected types and
+        dimensions.
+        """
+        [t_avg, time, lat, lon] = process_obs.calculate_temps(filename=TEST_FILE)
+        test_ds = process_obs.create_obs_dataset(t_avg, time, lat, lon)
         test_coords = [x for x in test_ds.coords]
 
         ds_pass = isinstance(test_ds, EXP_TYPES[0])
         lat_pass = (isinstance(test_ds['lat'].values[0], EXP_TYPES[1]) and len(test_ds['lat'].values) == EXP_DIMS[0])
         lon_pass = (isinstance(test_ds['lon'].values[0], EXP_TYPES[2]) and len(test_ds['lon'].values) == EXP_DIMS[1])
         time_pass = (isinstance(test_ds['time'].values[0], EXP_TYPES[3]) and len(test_ds['time'].values) == EXP_DIMS[2])
-    else:
-        ds_pass = False
-        lat_pass = False
-        lon_pass = False
-        time_pass = False
 
-    assert file_exists
-    assert ds_pass
-    assert lat_pass
-    assert lon_pass
-    assert time_pass
+        self.assertTrue( ds_pass)
+        self.assertTrue( lat_pass)
+        self.assertTrue( lon_pass)
+        self.assertTrue( time_pass)
 
 
-def main():
-    """Runs all tests for each of the functions in the subcomp_d module."""
-    test_convert_to_360()
-    test_calculate_temps()
-    test_create_obs_dataset()
-    test_save_dataset()
+    def test_save_dataset(self): 
+        """
+        Tests that the dataset was saved correctly and looks the same as the processed dataset.
+        """
+        # first delete the file if it already exists in the directory - zarr can't overwrite
+        dw.delete_zarr_files(data_dir=TEST_OUTPUT_DIR, regex=OUTPUT_FILE_NAME, file_type='')
 
+        process_obs.process_all_observations(data_path=TEST_DATA_DIR,
+                                            data_path_out=TEST_OUTPUT_DIR)
 
+        out_dir_files = glob.glob(TEST_OUTPUT_DIR + '*.zarr', recursive=True)
+        file_exists = OUTPUT_FILE in out_dir_files
+
+        if file_exists:
+            test_ds = xr.open_zarr(OUTPUT_FILE)
+            test_coords = [x for x in test_ds.coords]
+
+            ds_pass = isinstance(test_ds, EXP_TYPES[0])
+            lat_pass = (isinstance(test_ds['lat'].values[0], EXP_TYPES[1]) and len(test_ds['lat'].values) == EXP_DIMS[0])
+            lon_pass = (isinstance(test_ds['lon'].values[0], EXP_TYPES[2]) and len(test_ds['lon'].values) == EXP_DIMS[1])
+            time_pass = (isinstance(test_ds['time'].values[0], EXP_TYPES[3]) and len(test_ds['time'].values) == EXP_DIMS[2])
+        else:
+            ds_pass = False
+            lat_pass = False
+            lon_pass = False
+            time_pass = False
+
+        self.assertTrue( file_exists)
+        self.assertTrue( ds_pass)
+        self.assertTrue( lat_pass)
+        self.assertTrue( lon_pass)
+        self.assertTrue( time_pass)
+    
 if __name__ == '__main__':
-    main()
+    unittest.main()
