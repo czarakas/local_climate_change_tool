e05ad06c04b86905ebcb16e95d351abac5c0b8b6
smturbev <smturbev@uw.edu>
2019-12-08 21:45:50 +0000

    clean dashboard \n\n get rid of global mean and fixed some typos

--- local-climate-change-tool/phase2_dashboard_generator/util_panel.py
+++ local-climate-change-tool/phase2_dashboard_generator/util_panel.py
@@ -4,105 +4,68 @@ Sami Turbeville
 CSE 583 Project: CMIP 6 Local Climate Tool
 
 Module of useful functions for creating data that
-panel reads in. 
+panel reads in.
 """
 
-import pandas as pd
-import zarr
-import xarray as xr
 import sys
+import pandas as pd
+import xarray as xr
 
-dir_analysis = '../phase1_data_wrangler'
-sys.path.insert(0, dir_analysis)
+
+DIR_ANALYSIS = '../phase1_data_wrangler'
+sys.path.insert(0, DIR_ANALYSIS)
 import analysis_parameters
 
 
 DF = pd.read_csv('worldcities.csv')
 COUNTRIES = list(set(DF['country']))
-THIS_EXPERIMENT_ID = ['historical','ssp126', 'ssp370','ssp245','ssp585']
+THIS_EXPERIMENT_ID = ['historical', 'ssp126', 'ssp370', 'ssp245', 'ssp585']
 EXPERIMENT_KEYS = THIS_EXPERIMENT_ID.copy()
 EXPERIMENT_KEYS.append('historical_obs')
 
-def read_data(data_type):
+
+def read_data():
     """
-    Input: data_type - key word can be one of the following:
-            'dummy'
-            'global'
-            'real'
-            
-    Returns a dictionary for data_type with keys: 
+    Returns a dictionary for data_type with keys:
             historical
             ssp126
             ssp370
             ssp245
             ssp585
             historical_obs
-        Note, returned xarray for gloabl means has no lat lon
-        information. 
     """
-    
-    #initiate dictionary - which will match experiment names to data
     dict_timeseries = dict()
-
-    if data_type == 'dummy':
-        data_path = analysis_parameters.DIR_DUMMY_DATA
-        # Read in model data
-        for experiment_id in THIS_EXPERIMENT_ID:
-            filename = data_path + 'Zarr/dummyData_modelData_' + experiment_id + '.zarr'
-            ds = xr.open_zarr(filename)
-            dict_timeseries[experiment_id] = ds
-        # Read in observation data
-        filename = data_path + 'Zarr/dummyData_observationData' + '.zarr'
+    data_path = analysis_parameters.DIR_PROCESSED_DATA
+    # Read in model data
+    for experiment_id in THIS_EXPERIMENT_ID:
+        filename = data_path + 'model_data/modelData_tas_' + experiment_id + '.zarr'
         ds = xr.open_zarr(filename)
-        dict_timeseries['historical_obs'] = ds
+        dict_timeseries[experiment_id] = ds
+    # Read in observation data
+    filename = data_path + 'observation_data/historical_obs.zarr'
+    ds = xr.open_zarr(filename)
+    dict_timeseries['historical_obs'] = ds
 
-    elif data_type == 'global':
-        data_path = analysis_parameters.DIR_PROCESSED_DATA
-        # Read in model data
-        for experiment_id in THIS_EXPERIMENT_ID:
-            filename = data_path + 'model_data/global_mean_data/tas_' + experiment_id + '_GLOBALMEAN_STATS.zarr'
-            ds = xr.open_zarr(filename)
-            dict_timeseries[experiment_id] = ds
-        # Read in observation data
-        filename = data_path + 'observation_data/historical_obs.zarr'
-        ds = xr.open_zarr(filename)
-        dict_timeseries['historical_obs'] = ds
-
-    elif data_type == 'real':
-        data_path = analysis_parameters.DIR_PROCESSED_DATA
-        # Read in model data
-        for experiment_id in THIS_EXPERIMENT_ID:
-            filename = data_path + 'model_data/modelData_tas_' + experiment_id + '.zarr'
-            ds = xr.open_zarr(filename)
-            dict_timeseries[experiment_id] = ds
-        # Read in observation data
-        filename = data_path + 'observation_data/historical_obs.zarr'
-        ds = xr.open_zarr(filename)
-        dict_timeseries['historical_obs'] = ds
-
-    else:
-        raise Exception('''Input parameter should be one of the following:\n
-                        - dummy\n - real \n - global\nnot %s'''%(data))
     return dict_timeseries
 
 
 def create_country2city2latlon_dict():
     """
     Returns a nested dictionary. First key is country.
-        Second Key is city. The value is the lat lon 
+        Second Key is city. The value is the lat lon
         value that will be used to update the timeseries
-        plot. The first key will be a drop down menu and 
-        the second will be a drop down menu that depends 
-        on the input of the first. 
+        plot. The first key will be a drop down menu and
+        the second will be a drop down menu that depends
+        on the input of the first.
     """
     my_dict = dict()
     for country in COUNTRIES:
-        my_dict.update({country : dict()})
+        my_dict.update({country: dict()})
     for i in range(12959):
         coun = DF['country'].values[i]
         city = DF['city_ascii'].values[i]
         lat = DF['lat'].values[i]
         lon = DF['lng'].values[i]
-        latlon = [lat, lon]        
-        my_dict[coun].update({city : latlon})
+        latlon = [lat, lon]
+        my_dict[coun].update({city: latlon})
     return my_dict
