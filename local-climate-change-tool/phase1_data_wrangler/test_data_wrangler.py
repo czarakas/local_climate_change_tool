7b01107ae4187b98f8fcc8eba66aae9f77013063
Default user <jnug@uw.edu>
2019-12-08 20:11:16 +0000

    Added more tests for coverage. Still in progress

--- local-climate-change-tool/phase1_data_wrangler/test_data_wrangler.py
+++ local-climate-change-tool/phase1_data_wrangler/test_data_wrangler.py
@@ -3,6 +3,8 @@ import unittest
 import xarray as xr
 import data_wrangler as dw
 import analysis_parameters as params
+import unittest
+import pytest
 
 # Define directory and file names
 DIR_TESTING = params.DIR_DUMMY_DATA
@@ -18,18 +20,28 @@ DSET_DICT = dict()
 DSET_DICT[TEST_KEY1] = 57#xr.open_dataset(TESTING_DATA_DIR+TEST_KEY1+'.nc')
 DSET_DICT[TEST_KEY2] = 57#xr.open_dataset(TESTING_DATA_DIR+TEST_KEY2+'.nc')
 
-class test_subcomp_a(unittest.TestCase):
+class test_data_wrangler(unittest.TestCase):
     def test_print_time(self):
         dw.print_time()
         self.assertTrue(True)
-
-    def test_subcomp_a(self, xperiment_ID='ssp126'):
-        dset_dict = dw.subcomponent_a(print_statements_on=True)
+    
+    def test_subcomps(self):
+        pytest.main(["test_subcomp_a.py"])
+        pytest.main(["test_subcomp_b.py"])
+        pytest.main(["test_subcomp_c.py"])
+        pytest.main(["test_subcomp_d.py"])
+    
+    def test_delete_zarr(self):
         self.assertTrue(True)
+
+#     def test_subcomp_a(self, xperiment_ID='ssp126'):
+#         dset_dict = dw.subcomponent_a(print_statements_on=True)
+#         self.assertTrue(True)
 
     #def test_subcomp_b(ref_grid_key='TEST_KEY2',dset_dict=DSET_DICT,
     #                   print_statements_on=True, data_dir=DIR_TESTING+'raw_data/'):
     #    dw.subcomponent_b(ref_grid_key, dset_dict, print_statements_on, data_dir)
+
 
 if __name__ == "__main__":
     unittest.main()
