e5f70e4ccddabe9511e763c0ee91083e3e0e285c
smturbev <smturbev@uw.edu>
2019-12-09 04:39:07 +0000

    passed pylint/flake8

--- local_climate_change_tool/phase1_data_wrangler/subcomp_a_create_data_dict.py
+++ local_climate_change_tool/phase1_data_wrangler/subcomp_a_create_data_dict.py
@@ -7,22 +7,25 @@ import analysis_parameters as params
 
 DIR_CATALOG = params.DIR_CATALOG
 
-def create_data_dict(this_experiment_id, this_variable_id, this_table_id, this_grid_label):
+
+def create_data_dict(this_experiment_id, this_variable_id,
+                     this_table_id, this_grid_label):
     """
     Creates data dictionary
     """
 
-    col = intake.open_esm_datastore(DIR_CATALOG+"pangeo-cmip6.json")
+    col = intake.open_esm_datastore(DIR_CATALOG + "pangeo-cmip6.json")
 
-    cat = col.search(experiment_id=this_experiment_id, \
-                     table_id=this_table_id, \
-                     variable_id=this_variable_id, \
+    cat = col.search(experiment_id=this_experiment_id,
+                     table_id=this_table_id,
+                     variable_id=this_variable_id,
                      grid_label=this_grid_label)
     dataset_info = cat.df
 
-    dset_dict = cat.to_dataset_dict(zarr_kwargs={'consolidated': True, 'decode_times': False},
-                                    cdf_kwargs={'chunks': {}, 'decode_times': False})
-
+    dset_dict = cat.to_dataset_dict(zarr_kwargs={'consolidated': True,
+                                                 'decode_times': False},
+                                    cdf_kwargs={'chunks': {},
+                                                'decode_times': False})
     source_ids = cat.df['source_id']
     modelnames = list(set(source_ids))
 
